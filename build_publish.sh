ca55b0a735225811d5f6f6dd3a354f846d1f6712
praton1729 <prathu.baronia@oneplus.com>
2021-06-17 22:35:23 +0530

    Delete duplicate blog syncing script
    Signed-off-by: praton1729 <prathu.baronia@oneplus.com>

--- build_publish.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "Building production website...."
-
-JEKYLL_ENV=production bundle exec jekyll build
-
-echo "Publishing production website"
-
-rsync -r _site/* blog_machine:/home/yynj07hrhveh/public_html
