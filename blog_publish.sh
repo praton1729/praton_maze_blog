d110c9aa2ea6978ef88d0a2ac01d9e4c4e3279e8
Prathu Baronia <prathu.baronia@praton.me>
2021-07-11 19:48:40 +0530

    Cleanup: not publishing latest blog on praton.me for time being
    - Remove blog build cmd from this script
    - Remove blog pushing to praton.me server for time being.
    - Add progress indicator in rsync for blog.praton.xyz
    
    Signed-off-by: Prathu Baronia <prathu.baronia@praton.me>

--- blog_publish.sh
+++ blog_publish.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
 
-echo "Building production website...."
-
-JEKYLL_ENV=production bundle exec jekyll build
-
-echo "Publishing production website on praton.me"
-
-rsync -r --info=progress2 _site/* blog_machine:/home/tnn1sqgvuaxk/public_html
+#echo "Publishing production website on praton.me"
+#rsync -r --info=progress2 _site/* blog_machine:/home/tnn1sqgvuaxk/public_html
 
 echo "Publishing production website on blog.praton.xyz"
-rsync -r _site/* blog_vultr:/var/www/blog/
+rsync --info=progress2 -r _site/* blog_vultr:/var/www/blog/
